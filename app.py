01167b53d3b56f14d8df2f7e73c47af83ad47bed
nishizawareo <r.nishizawa.sys22@morijyobi.ac.jp>
2023-06-29 15:09:26 +0900

    新規登録・ログイン機能の実装

--- app.py
+++ app.py
@@ -1,15 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for
+import db
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def top():
     return render_template('index.html')
 
 @app.route('/register')
-def user_register():
+def user_register_form():
     return render_template('user_register.html')
 
+@app.route('/register_exe', methods=['POST'])
+def user_register_exe():
+    name = request.form.get('name')
+    password = request.form.get('password')
+    birth = request.form.get('birth')
+    
+    count = db.insert_user(name, password, birth)
+    
+    if count == 1:
+        msg = '登録が完了しました。'
+        return redirect(url_for('user_login_index', msg=msg))
+    
+    else:
+        error = '登録に失敗しました。'
+        return render_template('user_register.html', error=error)
+
+@app.route('/login')
+def user_login_index():    
+    msg = request.args.get('msg')
+    if msg == None:
+        return render_template('user_login.html')
+    else:
+        return render_template('user_login.html', msg=msg)
+
+@app.route('/login', methods=['POST'])
+def user_login():
+    name = request.form.get('name')
+    password = request.form.get('password')
+    
+    if db.login(name, password):
+        return redirect(url_for('mypage'))
+    else:
+        error = '名前またはパスワードが違います。'
+        input_data = {'name':name, 'password':password}
+        return render_template('user_login.html', error=error, data=input_data)
+    
+@app.route('/my_top', methods=['GET'])
+def mypage():
+    return render_template('mypage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
