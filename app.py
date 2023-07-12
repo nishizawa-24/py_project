5e2de55fe5de16ae6c30e1237fda26b8f5c90820
nishizawareo <r.nishizawa.sys22@morijyobi.ac.jp>
2023-07-12 13:46:28 +0900

    様々な機能の追加

--- app.py
+++ app.py
@@ -42,8 +42,9 @@ def user_login():
     name = request.form.get('name')
     password = request.form.get('password')
     
-    if db.login(name, password):
+    if db.login(name, password)[0]:
         session['user'] = True
+        session['user_info'] = db.login(name, password)[1]
         return redirect(url_for('mypage'))
     else:
         error = '名前またはパスワードが違います。'
@@ -53,6 +54,7 @@ def user_login():
 @app.route('/logout')
 def logout():
     session.pop('user', None)
+    session.pop('user_info', None)
     return redirect(url_for('top'))
     
 @app.route('/top', methods=['GET'])
@@ -74,8 +76,12 @@ def show_all_book():
 def book_detail():
     book_id = request.form.get('book_id')
     book = db.get_book(book_id)
+    if request.form.get('return_book') == 'True':
+        return_book = True
+    else:
+        return_book = False
     if 'user' in session:
-        return render_template('book_detail.html', book=book)
+        return render_template('book_detail.html', book=book, return_book=return_book)
     else:
         return redirect(url_for('top'))
 
@@ -85,14 +91,60 @@ def book_search():
     books = db.get_searched_books(keyword)
     msg = '図書が見つかりませんでした。'
     
-    if 'user' in session:
-        return redirect(url_for('top'))
-    
-    if books == []:
+    if 'user' not in session:
+        return redirect(url_for('top'))    
+    elif books == []:
         return render_template('search_result.html', keyword=keyword, books=books, msg=msg)
     else:
         return render_template('search_result.html', keyword=keyword, books=books, msg=None)
 
+@app.route('/borrow_confirm', methods=['POST'])
+def book_borrow_confirm():
+    book_id = request.form.get('book_id')
+    book = db.get_book(book_id)
+    if 'user' in session and db.borrow_confirm_book_pass(book_id):
+        msg = 'この図書を借りますか？'
+        flg = 'yes'
+        return render_template('book_borrow.html', book=book, msg=msg, flg=flg)
+    elif 'user' in session and db.borrow_confirm_user(session['user_info']) and db.borrow_confirm_book(book_id):
+        msg = 'この図書を借りますか？'
+        flg = 'yes'
+        return render_template('book_borrow.html', book=book, msg=msg, flg=flg)
+    elif 'user' in session:
+        msg = '現在この図書を借りることはできません。'
+        flg = 'no'
+        return render_template('book_borrow.html', book=book, msg=msg, flg=flg)
+    else:
+        return redirect(url_for('top'))
     
+@app.route('/borrow', methods=['POST'])
+def book_borrow():
+    book_id = request.form.get('book_id')
+    book = db.get_book(book_id)
+    if 'user' in session and db.borrow_book(session['user_info'], book_id) == 1 and db.update_current_books_borrowed(session['user_info']) == 1:
+        msg = 'この図書を借りました。'
+        flg = 'success'
+        return render_template('book_borrow.html', book=book, msg=msg, flg=flg)
+    elif 'user' in session:
+        msg = 'この図書を借りることができませんでした。'
+        flg = 'no'
+        return render_template('book_borrow.html', book=book, msg=msg, flg=flg)
+    else:
+        return redirect(url_for('top'))
+    
+@app.route('/list_borrowed', methods=['GET'])
+def book_list_borrowed():
+    user_id = session['user_info']
+    book_id_list = db.borrowed_book_id_list(user_id)
+    book_list = []
+    return_book = True
+    for num in book_id_list:
+        row = db.get_book([num][0])
+        book_list.append(row)
+    if 'user' in session:
+        return render_template('book_list.html', book_list=book_list, return_book=return_book)
+    else:
+        return redirect(url_for('top'))
+
 if __name__ == '__main__':
     app.run(debug=True)
